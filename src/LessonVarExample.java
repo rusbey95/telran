77e10b9f1566b97966b8c1911c16e70cd05e1991
rusbey95 <rusikbey95@gmail.com>
2022-12-07 22:33:34 +0100

    1 java lesson

---
+++ src/LessonVarExample.java
@@ -0,0 +1,33 @@
+public class LessonVarExample {
+    public static void main(String[] args) {
+
+        //Vars type int
+        int x = 25;
+        int y = 20;
+        System.out.println(x);
+
+        //Vars type double
+        double pi = Math.PI;
+        double result = x * pi;
+        System.out.println("Pi = " + pi);
+        System.out.println("Result = " + result);
+
+        //Vars type boolean
+        boolean check;
+        check = x >= y;
+        System.out.println(check);
+
+        boolean b1 = true;
+        boolean b2 = false;
+        System.out.println(b1 & b2);
+        System.out.println(b1 || b2);
+        System.out.println(!b2);
+
+        int a = 10;
+        int b = 4;
+
+        float tet = a / b;
+        System.out.println(tet);
+
+    }
+}
---
+++ src/test1/test1.java
@@ -0,0 +1,9 @@
+package test1;
+
+public class test1 {
+    public static void main(String[] args) {
+        byte var1 = 10;
+        char var2 = 234;
+        System.out.println(var2 + var1);
+    }
+}
